2f07635e73bba20ffe7324b86fbc56f185a1ac4f
yangshoong <crinaeae83@gmail.com>
2024-05-31 01:15:08 +0900

    프로젝트 기본셋팅

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // req.body가 객체로 인식이 됩니다
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("mongoose connected"))
+  .catch((err) => console.log("DB connection fail", err));
+
+app.listen(process.env.PORT || 5050, () => {
+  console.log("server on");
+});
